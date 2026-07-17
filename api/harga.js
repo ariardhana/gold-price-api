ca0d5f85af256c6c9c67fc1bc72c2473451c5e9d
ariardhana <aright.abe@gmail.com>
2026-07-17 10:10:21 +0700

    Update harga.js

--- api/harga.js
+++ api/harga.js
@@ -1,44 +1,30 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://www.antamgold.com/fetch-price.php");
-
     const json = await response.json();
-
-    if (!json.status) {
-      throw new Error("Data tidak tersedia");
-    }
 
     const tahun = Object.keys(json.data)[0];
     const data = json.data[tahun];
 
-    function toNumber(str) {
-      return Number(str.replace(/[^\d]/g, ""));
+    const hasil = {};
+
+    for (const key in data) {
+      hasil[key] = {
+        harga: Number(data[key].harga.replace(/[^\d]/g, "")),
+        buyback: Number(data[key].harga_buyback.replace(/[^\d]/g, ""))
+      };
     }
 
     res.status(200).json({
       status: "ok",
       update: new Date().toISOString(),
-
-      gram1: {
-        harga: toNumber(data["1.0"].harga),
-        buyback: toNumber(data["1.0"].harga_buyback)
-      },
-
-      gram5: {
-        harga: toNumber(data["5.0"].harga),
-        buyback: toNumber(data["5.0"].harga_buyback)
-      },
-
-      gram10: {
-        harga: toNumber(data["10.0"].harga),
-        buyback: toNumber(data["10.0"].harga_buyback)
-      }
+      harga: hasil
     });
 
-  } catch (err) {
+  } catch (e) {
     res.status(500).json({
       status: "error",
-      message: err.message
+      message: e.toString()
     });
   }
 }
